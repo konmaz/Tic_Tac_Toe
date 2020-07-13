f052f38a402eaa0fa1f9aad26f8be22f1d7a2f27
Konstantinos <58148103+konmaz@users.noreply.github.com>
2020-07-14 01:25:54 +0300

    ...

---
+++ code.js
@@ -0,0 +1,29 @@
+let pos = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+
+function clk(p1) {
+  console.log("clicked " + p1)
+  draw(p1)
+}
+
+function draw(pos,symbol) {
+  switch (pos) {
+    case 'k1':
+      document.getElementById('k1_img').style.visibility = 'visible';
+      document.getElementById('k1_img').fir
+
+      
+
+      
+      
+      
+      
+      break;
+  
+    default:
+      break;
+  }
+}
+
+
