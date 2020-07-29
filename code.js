7ec59c3279756818249533a855e1d3f81384f4c2
Konstantinos <58148103+konmaz@users.noreply.github.com>
2020-07-29 17:40:47 +0300

    Player swap working

--- code.js
+++ code.js
@@ -2,42 +2,56 @@ var board = [0, 0, 0,
   0, 0, 0,
   0, 0, 0]
 
-var player_turn = false;
-
+var player_turn = false; //false = O's begin first , true X's begin first
+if (player_turn)
+{
+  document.getElementById("o_score").style.opacity = "0.4";
+}
+else
+{
+  document.getElementById("x_score").style.opacity = "0.4";
+}
 // event player clicked on box
 function clk(box_pos) {
-
-  if (board[box_pos[1]]) { //check if the spot is empty
+  has_anybody_won();
+  console.log(board);
+  if (board[box_pos[1]-1]) { //check if the spot is empty
     console.log('nothing');
     return;
   }
   else {
-    board[box_pos[1]] = 1;
+    
     if (player_turn) {
+      board[box_pos[1]-1] = 'X';
       draw(box_pos, 'x' + box_pos[1]);
       player_turn = false;
+      document.getElementById("x_score").style.opacity = "0.4";
+      document.getElementById("o_score").style.opacity = "1.0";
+      
     }
     else {
+      board[box_pos[1]-1] = 'O';
       draw(box_pos, 'o' + box_pos[1]);
       player_turn = true;
+      document.getElementById("x_score").style.opacity = "1.0";
+      document.getElementById("o_score").style.opacity = "0.4";
     }
   }
 }
 
+//just draws the X's and O's
 function draw(pos, source) {
   console.log(pos, source)
   switch (pos) {
 
     case 'k1':
-
-
+      
       myVivus1 = new Vivus(source, { type: 'oneByOne', duration: 25 });
       document.getElementById(source).style.visibility = 'visible';
       myVivus1.play();
       break;
 
     case 'k2':
-
 
       myVivus2 = new Vivus(source, { type: 'oneByOne', duration: 25 });
       document.getElementById(source).style.visibility = 'visible';
@@ -99,4 +113,10 @@ function draw(pos, source) {
   }
 }
 
+function has_anybody_won(){
+  if (board[6] == board[7] ==board[8])
+    {console.log(board[7]+' has won')
+  } 
+
+}
 
